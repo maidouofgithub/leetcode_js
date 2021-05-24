2de968b5f8f9daf6d18c36c158544775cbef38a1
奉伟 <landwind1180@gmail.com>
2021-05-24 16:59:43 +0800

    f

---
+++ src/38.外观数列.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=38 lang=javascript
+ *看着有压缩算法的意思
+ * [38] 外观数列
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+
+};
+// @lc code=end
+
